5ed1b55d2f69421732cd1e5bbb21a7574d826179
EnriqueVe <48701249+EnriqueVe@users.noreply.github.com>
2019-05-08 00:40:07 -0500

    sumar3

--- js/acciones.js
+++ js/acciones.js
@@ -1,26 +1,28 @@
-//JavaScript Document
+// JavaScript Document
 
 $(document).ready(function(e){
 	document.addEventListener("deviceready",function(){
-		
-		$('#izquierda').on("swipeleft",function(){
-			navigator.notification.alert("Deslizo a la izquierda",function(){"Aplicacion ","Aceptar"});
-});
 
-$('#derecha').on("swiperight",function(){
-	navigator.notification. confirm ("¿Que quieres hacer?",function(opt){
-	switch(opt)
-	{
-		case 1:
-		navigator.notification.beep(1);
-		break;
-		
-		case 2:
-		navigator.notification.vibrate(1000);
-		break;
-	}
-	},"Aplicacion ","Beep,Vibrar,Cancelar");
-	
-});
+	$('#Izquierda').on("swipeleft",function(){
+	navigator .notification. alert("Deslizo a la Izquierda",function(){"aplicacion ","aceptar"});
+	});
+
+$('#Derecha').on("swiperight",function(){
+	navigator .notification. confirm("¿Que quieres hacer?",function(opt){
+		switch(opt)
+		{
+			case 1:
+			navigator.notification.beep(1);
+			break;
+
+			case 2:
+			navigator.notification.vibrate(1000);
+			break;
+		}
+	},"aplicacion ","beep,vibrar,cancelar");
+
+	});	
 	},false);
-});
+	
+});//document
+		
